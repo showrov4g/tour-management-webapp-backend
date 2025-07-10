3b2affda982f07f787e60d0dc9e0ab08806d046f
showrov4g <showrovg00@gmail.com>
2025-07-11 00:21:35 +0600

    create user api making

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,5 +1,17 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from "express";
 
-const createUser = await (req: Request, res: Response)=>{
-
+const createUser = async (req: Request, res: Response)=>{
+    try {
+        const {name, email} = req.body;
+        
+        
+    } catch (error: any) {
+        console.log(error);
+        res.status(400).json({
+            message: `Something getting wrong ${error.message}`,
+            error
+        })
+    }
 }
