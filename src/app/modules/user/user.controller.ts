c955f6f55d346d676b2559365e355c1eda4bb8d8
showrov4g <showrovg00@gmail.com>
2025-07-11 00:30:10 +0600

    create user api create

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,15 +1,25 @@
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from "express";
+import { User } from "./user.model";
+import httpStatus from "http-status-codes";
+
 
 const createUser = async (req: Request, res: Response)=>{
     try {
         const {name, email} = req.body;
-        
-        
+        const user = await User.create({
+            name,
+            email
+        })
+        res.status(httpStatus.CREATED).json({
+            message: 'User created Successfully',
+            user
+        })
+
     } catch (error: any) {
         console.log(error);
-        res.status(400).json({
+        res.status(httpStatus.BAD_REQUEST).json({
             message: `Something getting wrong ${error.message}`,
             error
         })
