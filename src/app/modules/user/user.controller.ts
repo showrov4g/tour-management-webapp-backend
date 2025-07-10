2bb1f684f2d49fbe01c82d96887ea790dcfe3933
showrov4g <showrovg00@gmail.com>
2025-07-10 23:06:05 +0600

    user interface making

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,3 +1,5 @@
+import { Types } from "mongoose";
+
 export enum Role {
     SUPPER_ADMIN = "SUPPER_ADMIN",
     ADMIN = "ADMIN",
@@ -7,23 +9,28 @@ export enum Role {
 
 
 // auth provider 
-export interface IAuthProvider{
+export interface IAuthProvider {
     provider: string; // "google", "Credential"
     providerId: string;
 }
 
+export enum isActive {
+    ACTIVE = "ACTIVE",
+    INACTIVE = "INACTIVE",
+    BLOCKED = "BLOCKED"
+}
 
 export interface IUser {
     user: string;
     email: string;
-    password?: string;
-    phone?: string;
-    picture?: string;
-    address?: string;
-    isDeleted?: string;
-    isActive?: string;
+    password ?: string;
+    phone ?: string;
+    picture ?: string;
+    address ?: string;
+    isDeleted ?: string;
+    isActive ?: isActive;
     role: "USER" | "ADMIN" | "GUIDE";
-    auth: IAuthProvider;
-    bookings?:
-    guides?:
+    auth: IAuthProvider[];
+    bookings ?: Types.ObjectId[];
+    guides ?: Types.ObjectId[];
 }
