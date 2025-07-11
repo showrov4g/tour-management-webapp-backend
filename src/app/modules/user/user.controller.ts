e479712916ea7646057ef34c035c5c8a9f24fb7e
showrov4g <showrovg00@gmail.com>
2025-07-11 17:31:18 +0600

    learning new thing

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,15 +3,13 @@
 import { Request, Response } from "express";
 import { User } from "./user.model";
 import httpStatus from "http-status-codes";
+import { UserServices } from "./user.service";
 
 
 const createUser = async (req: Request, res: Response)=>{
     try {
-        const {name, email} = req.body;
-        const user = await User.create({
-            name,
-            email
-        })
+        const user = await UserServices.createUser(req.body)
+        
         res.status(httpStatus.CREATED).json({
             message: 'User created Successfully',
             user
