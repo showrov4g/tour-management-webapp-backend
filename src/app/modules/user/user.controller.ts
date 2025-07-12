105e8f77563d36963a0058c0a2d1916e875bc2a0
showrov4g <showrovg00@gmail.com>
2025-07-12 10:25:16 +0600

    trycatch redendancy remove

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -6,29 +6,36 @@ import { User } from "./user.model";
 import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
 import AppError from "../../errorClass/AppError";
+import { catchAsync } from "../../utils/catchAsync";
 
 // handler 
-type AsyncHandler = (req: Request, res: Response, next: NextFunction)=>Promise<void>
-
-const catchAsync = (fn: AsyncHandler)
 
 
-
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-
+const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction)=>{
         const user = await UserServices.createUser(req.body)
 
         res.status(httpStatus.CREATED).json({
             message: 'User created Successfully',
             user
         })
-        throw new AppError(httpStatus.BAD_REQUEST, "fake error")
-    } catch (err: any) {
-        console.log(err);
-        next(err)
-    }
-}
+})
+
+
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+
+//         const user = await UserServices.createUser(req.body)
+
+//         res.status(httpStatus.CREATED).json({
+//             message: 'User created Successfully',
+//             user
+//         })
+//         throw new AppError(httpStatus.BAD_REQUEST, "fake error")
+//     } catch (err: any) {
+//         console.log(err);
+//         next(err)
+//     }
+// }
 
 // get all user controllers 
 const getALlUser = async(req: Request, res: Response, next: NextFunction) =>{
