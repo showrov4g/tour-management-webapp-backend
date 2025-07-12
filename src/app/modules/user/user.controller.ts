b55dafc7bd5038e350abd2a68488bdd97d29e7d4
showrov4g <showrovg00@gmail.com>
2025-07-12 09:59:14 +0600

    global error handle

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express";
@@ -5,6 +6,12 @@ import { User } from "./user.model";
 import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
 import AppError from "../../errorClass/AppError";
+
+// handler 
+type AsyncHandler = (req: Request, res: Response, next: NextFunction)=>Promise<void>
+
+const catchAsync = (fn: AsyncHandler)
+
 
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
@@ -23,8 +30,19 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     }
 }
 
+// get all user controllers 
+const getALlUser = async(req: Request, res: Response, next: NextFunction) =>{
+    try {
+        const users =await UserServices.getAllUser(); 
+        return users;
+    } catch (error: any) {
+        console.log(error);
+        next(error)
+    }
+}
 
 
 export const UserControllers = {
-    createUser
+    createUser,
+    getALlUser
 }
