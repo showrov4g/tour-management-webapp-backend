dd9f1aad9d7a503c1f388b7acce24c918b5bc1c6
showrov4g <showrovg00@gmail.com>
2025-07-14 06:52:35 +0600

    make update user zod schema

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,26 +1,18 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { UserControllers } from "./user.controller";
-import z from "zod";
-
+import { createUserZodSchema } from "./user.validation";
+import { AnyZodObject, ZodObject } from "zod";
+import { validateRequest } from "../../middlewars/validate";
 
 
 const router = Router();
-const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-router.post("/register",
-    (req: Request, res: Response, next: NextFunction) => {
-        const createUserZodSchema = z.object({
-            name: z.string({ invalid_type_error: "name must be string" }).min(2, { message: "Name is too short" }).max(50, { message: "Name is too big" }),
-            user: z.string(),
-            email: z.string().email().min(5, { message: "email must be at last 5 character " }).max(20, { message: "email not more than 20 character" }),
-            password: z.string().min(8).regex(passwordRegex, { message: "Password must include uppercase, lowercase, number, and special character" }),
-            phone: z.string({ invalid_type_error: "number must be string" }).regex(/^(?:\+88|88)?01[3-9]\d{8}$/, { message: "phone number must be in Bangladeshi Standard" }),
-            address: z.string({ invalid_type_error: "Address must be string" }).max(200, { message: "address must be under 200 character" }).optional()
+// function 
 
-        })
-        req.body = 
-    },
-    UserControllers.createUser);
+
+
+
+router.post("/register", validateRequest(createUserZodSchema), UserControllers.createUser);
 
 
 router.get("/all-users", UserControllers.getAllUser)
---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,36 @@
+import z from "zod";
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+export const createUserZodSchema = z.object({
+    name: z.string({ invalid_type_error: "name must be string" })
+        .min(2, { message: "Name is too short" })
+        .max(50, { message: "Name is too big" }),
+    user: z.string(),
+    email: z.string().email()
+        .min(5, { message: "email must be at last 5 character " })
+        .max(20, { message: "email not more than 20 character" }),
+    password: z.string()
+        .min(8)
+        .regex(passwordRegex, { message: "Password must include uppercase, lowercase, number, and special character" }),
+    phone: z.string({ invalid_type_error: "number must be string" })
+        .regex(/^(?:\+88|88)?01[3-9]\d{8}$/, { message: "phone number must be in Bangladeshi Standard" }),
+    address: z.string({ invalid_type_error: "Address must be string" }).max(200, { message: "address must be under 200 character" }).optional()
+
+});
+
+export const updateUserZodSchema = z.object({
+    name: z.string({ invalid_type_error: "name must be string" })
+        .min(2, { message: "Name is too short" })
+        .max(50, { message: "Name is too big" }),
+    user: z.string(),
+    email: z.string().email()
+        .min(5, { message: "email must be at last 5 character " })
+        .max(20, { message: "email not more than 20 character" }),
+    password: z.string()
+        .min(8)
+        .regex(passwordRegex, { message: "Password must include uppercase, lowercase, number, and special character" }),
+    phone: z.string({ invalid_type_error: "number must be string" })
+        .regex(/^(?:\+88|88)?01[3-9]\d{8}$/, { message: "phone number must be in Bangladeshi Standard" }),
+    address: z.string({ invalid_type_error: "Address must be string" }).max(200, { message: "address must be under 200 character" }).optional()
+
+});
