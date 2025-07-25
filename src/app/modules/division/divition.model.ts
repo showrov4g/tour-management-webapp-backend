fd631542904971cfb70bc1180d42c1a1cbfb896f
showrov4g <showrovg00@gmail.com>
2025-07-25 11:10:37 +0600

    createDivision serveice making

--- src/app/modules/division/divition.model.ts
+++ src/app/modules/division/divition.model.ts
@@ -10,5 +10,35 @@ const divisionSchema = new Schema<IDivition>({
     timestamps: true,
 })
 
+divisionSchema.pre("save", async function (next){
+    if(this.isModified("name")){
+        const baseSlug = this.name.toLocaleLowerCase().split('').join("_");
+        let slug = `${baseSlug}-division`
+        let counter = 0;
+        while(await Division.exists({slug})){
+            slug = `${slug}-${counter}`
+        }
+        this.slug = slug;
+    }
+    next()
+})
+
+
+divisionSchema.pre("findOneAndUpdate", async function(next){
+    const division = this.getUpdate() as Partial<IDivition>
+   if(division.name){
+    const baseSlug = division.name.toLocaleLowerCase().split(" ").join("_")
+    let slug = `${baseSlug}-division`;
+    let counter = 0;
+    while(await Division.exists({slug})){
+        slug = `${slug}-{counter++}`
+    }
+    division.slug = slug;
+   }
+   this.setUpdate(division)
+   next();
+
+})
+
 
 export const Division = model<IDivition>("Division", divisionSchema)
